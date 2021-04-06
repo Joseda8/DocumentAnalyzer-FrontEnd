48f93a773ab26852f4c06dab31c540f7f4a1a5fa
Arturo Cordoba <arturocv16@gmail.com>
2021-04-06 15:20:20 -0600

    Authorization header added to all outgoing requests

--- DocumentAnalyzer/doc-frontend/src/index.js
+++ DocumentAnalyzer/doc-frontend/src/index.js
@@ -6,6 +6,14 @@ import axios from 'axios';
 import AuthService from './components/auth/auth-service'
 
 ReactDOM.render(<App />, document.getElementById("root"));
-axios.interceptors.request.use();
+axios.interceptors.request.use((config) => {
+    const token = AuthService.token;
+
+    // Checks if the token is invalid
+    if(!token) return config;
+
+    config.headers.Authorization = 'Bearer ' + token;
+    return config;
+});
 
 AuthService.autoLogin();
