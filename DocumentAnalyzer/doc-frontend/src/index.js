3a0fe2bffc06230cb2e5aaa9f0be931a3bfefd9d
Arturo Cordoba <arturocv16@gmail.com>
2021-04-06 14:28:34 -0600

    Login request to the api & token storage

--- DocumentAnalyzer/doc-frontend/src/index.js
+++ DocumentAnalyzer/doc-frontend/src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from 'axios';
+
+import AuthService from './components/auth/authservice'
 
 ReactDOM.render(<App />, document.getElementById("root"));
+axios.interceptors.request.use();
+
+AuthService.autoLogin();
