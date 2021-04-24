384b0ef848be7f560320824c7ef6383922ada5ee
Arturo Cordoba <arturocv16@gmail.com>
2021-04-23 18:35:49 -0600

    Signup request with the IIS url

--- DocumentAnalyzer/doc-frontend/src/components/auth/signup/signup-service.js
+++ DocumentAnalyzer/doc-frontend/src/components/auth/signup/signup-service.js
@@ -1,10 +1,10 @@
 import axios from "axios";
+import { AUTHAPI_URL } from '../../../helpers/constants';
 
 export class SignupService {
-  static AUTHAPI_URL = "http://localhost:16293/";
 
   static signupRequest(fullName, email, password) {
-    return axios.post(this.AUTHAPI_URL + "Signup", {
+    return axios.post(AUTHAPI_URL + "Signup", {
       fullName: fullName,
       email: email,
       password: btoa(password), // The password is encrypted in base 64
